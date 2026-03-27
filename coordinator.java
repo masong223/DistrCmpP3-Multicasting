878ff84eaaab4f283c05eb7350ab70abe481477e
Bret Zubieta <bzubietap@gmail.com>
2026-03-27 12:00:33 -0400

    Coordinator commands

--- coordinator.java
+++ coordinator.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.HashMap;
 import java.util.Scanner;
+
 public class Coordinator {
     public static void main(String[] args) {
         File configFile = new File(args[0]);
@@ -25,6 +26,7 @@ public class Coordinator {
     } catch (FileNotFoundException e) {
         System.out.println("Configuration file not found: " + args[0]);
     }
+    
     int listenerPort = port;
     new Thread(() -> {
         try (ServerSocket serverSocket = new ServerSocket(listenerPort)) {
@@ -38,23 +40,33 @@ public class Coordinator {
     }).start();
     }
 
+
     private static void handleClient(Socket clientSocket, HashMap<Integer, Boolean> userStatus) {
         try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+
             String message;
             while ((message = in.readLine()) != null) {
                 switch (message.split(" ")[0]) {
                     case "register":
-                        userStatus.put(Integer.parseInt(message.split(" ")[3]), true);
+                        userStatus.put(Integer.parseInt(message.split(" ")[2]), true);
+                        String responseReg = "ACK" + message.split("")[2];
+                        out.println(responseReg);
                         break;
                     case "deregister":
                         userStatus.remove(Integer.parseInt(message.split(" ")[1]));
+                        String responseDereg = "ACK" + message.split(" ")[1] ;
+                        out.println(responseDereg);
                         break;
                     case "disconnect":
                         userStatus.put(Integer.parseInt(message.split(" ")[1]), false);
+                        String responseDisc = "ACK" + message.split(" ")[1];
+                        out.println(responseDisc);
                         break;
                     case "reconnect":
-                        userStatus.put(Integer.parseInt(message.split(" ")[1]), true);
+                        userStatus.put(Integer.parseInt(message.split(" ")[2]), true);
+                        String responseReconnect = "ACK" + message.split(" ")[2];
+                        out.println(responseReconnect);
                         break;
                     case "msend":
                         userStatus.forEach((userId, isOnline) -> {
@@ -70,5 +82,7 @@ public class Coordinator {
         } catch (IOException e) {
             e.printStackTrace();
         }
+   
     }
+
 }
